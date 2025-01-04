dfe2bc3a41d8a901679bea8ecee163b4a3bca759
Jar0705 <156736264+Jar0705@users.noreply.github.com>
2025-01-04 17:53:25 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-function addToCart(productName) {
-    alert(productName + " berhasil ditambahkan ke keranjang!");
-}
-
-document.getElementById('loginForm').addEventListener('submit', function (e) {
+document.getElementById('loginForm')?.addEventListener('submit', function (e) {
     e.preventDefault();
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
 
     if (username === "admin" && password === "admin123") {
-        alert("Login berhasil! Selamat datang, " + username + "!");
+        alert("Login berhasil!");
+        window.location.href = "dashboard.html";
     } else {
-        alert("Login gagal. Silakan periksa username atau password Anda.");
+        alert("Login gagal. Periksa username atau password Anda.");
     }
 });
+
+function addToCart(productName) {
+    alert(productName + " berhasil ditambahkan ke keranjang!");
+}
