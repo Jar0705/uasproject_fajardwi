b8865fd3c32df80718e0eb6af36eec68ec9652e1
Jar0705 <156736264+Jar0705@users.noreply.github.com>
2025-01-06 10:02:52 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const buttons = document.querySelectorAll('.item button');
-    buttons.forEach(button => {
-        button.addEventListener('click', () => {
-            alert('Produk berhasil ditambahkan ke keranjang!');
-        });
-    });
-});
+function addToCart(productName) {
+    alert(`${productName} telah ditambahkan ke keranjang!`);
+}
