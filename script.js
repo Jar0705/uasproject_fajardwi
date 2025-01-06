2fe17c489c9aa5e06724e4e1e9047d915d0b2e18
Jar0705 <156736264+Jar0705@users.noreply.github.com>
2025-01-06 10:12:26 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 function addToCart(productName) {
     alert(`${productName} telah ditambahkan ke keranjang!`);
 }
+
+function searchProduct() {
+    const query = document.querySelector('.search-bar input').value;
+    if (query) {
+        alert(`Hasil pencarian untuk: "${query}"`);
+    } else {
+        alert('Silakan masukkan kata kunci untuk mencari produk.');
+    }
+}
